64c6d28b40f7386d9b02817c84af0063019753c9
Favour Akomolafe <akomolafefavour@gmail.comm>
2022-07-19 20:39:56 +0100

    target blank

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -111,26 +111,26 @@ export const Contact = () => {
           </h2>
           <div className="space-y-7 font-semibold text-md text-gray-400 w-fit">
             <Link href={'https://www.github.com/hayormikun'}>
-              <a className="flex items-center">
+              <a target={"_blank"} className="flex items-center">
                 <FaGithub className="w-6 mr-2 text-black" /> Github
               </a>
             </Link>
             <Link
               href={'https://www.linkedin.com/in/favour-akomolafe-7345b9114/'}
             >
-              <a className="flex items-center">
+              <a target={"_blank"} className="flex items-center">
                 <FaLinkedinIn className="w-6 mr-2 text-blue-400" /> LinkedIn
               </a>
             </Link>
 
             <Link href={'mailto:akomolafefavour@gmail.com'}>
-              <a className="flex items-center">
+              <a target={"_blank"} className="flex items-center">
                 <FaMailBulk className="w-6 mr-2 text-zinc-400" /> Mail
               </a>
             </Link>
 
             <Link href={'https://www.twitter.com/FavorAkomolafe'}>
-              <a className="flex items-center">
+              <a target={"_blank"} className="flex items-center">
                 <FaTwitter className="w-6 mr-2 text-blue-400" /> Twitter
               </a>
             </Link>

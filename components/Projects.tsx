230f52f5ae37e7558e2c2454c4629794d9e69ed5
Favour Akomolafe <akomolafefavour@gmail.comm>
2022-07-20 19:35:36 +0100

    content for projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -28,14 +28,16 @@ export const Projects = () => {
 
             <div className="block p-5">
               <h2 className="text-blue-600 text-lg font-semibold mb-3">
-                Favour Akomolafe
+                Favour Akomolafe Portfolio
               </h2>
-              <p className="text-gray-400">
-                The portfolio site of favour akomolafe used to showcase past
-                projects, expertise and skills.
-                <br />
-                <br /> Visitors can send a message discussing their next project
-                or schedule a meeting with him on this site.
+              <p className="text-gray-400 font-normal">
+                This portfolio site is aimed at showcasing completed projects by
+                Favour Akomolafe. <br /> <br />Favour Akomolafe is a software
+                developer at Behruz Hybrid, a software development company aimed
+                at offering secure, scalable and desired products to clients.
+                
+                <br /> <br /> Visitors can send a message discussing their next
+                project or schedule a meeting with him via this site.
               </p>
             </div>
           </div>
@@ -55,10 +57,12 @@ export const Projects = () => {
               <h2 className="text-teal-400 text-lg font-semibold mb-3">
                 Madcuisines
               </h2>
-              <p className="text-gray-400">
-                A meal booking app customers can order food from madcuisines and
-                get their food delivered to them timely, the admin panel aids
-                monitoring of products and orders.
+              <p className="text-gray-400 font-normal">
+                Madcuisines is a Uk meal booking app where customers order their
+                desired meals and get it delivered to them timely at their
+                desired locations. <br /> <br /> Products are orders are
+                monitored and managed by authorized personnels via Madcuisines
+                admin panel.
               </p>
             </div>
           </div>
@@ -78,11 +82,13 @@ export const Projects = () => {
               <h2 className="text-red-400 text-lg font-semibold mb-3">
                 Saviours Assembly
               </h2>
-              <p className="text-gray-400">
-                Saviours Assembly is a bible believing church located in
-                nigeria. this web app helps church members listen to audio
-                messages and read daily devotionals updated by the church
-                management.
+              <p className="text-gray-400 font-normal">
+                Saviours Assembly is a Bible believing church with faith and
+                creed centred on Christ and the reality of what He has done in
+                his death, burial and resurrection. <br />
+                <br />
+                Members of the church have access to audio sermons and daily
+                devotions on the official website of Saviours Assembly.
               </p>
             </div>
           </div>
@@ -102,11 +108,12 @@ export const Projects = () => {
               <h2 className="text-blue-400 text-lg font-semibold mb-3">
                 Medvista Diagnostics
               </h2>
-              <p className="text-gray-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-                iure similique repudiandae, autem numquam ipsam aperiam facilis
-                necessitatibus alias quam ducimus voluptatum recusandae minima
-                expedita ad itaque nam voluptas odit?
+              <p className="text-gray-400 font-normal">
+                Medvista Diagnostic Services is Nigeria's fastest growing
+                medical diagnostics centre. They render quality and affordable
+                healthcare services to patients. <br /> <br /> Patients can
+                schedule an appointment, see a list of rendered services and
+                contact information on the web app.
               </p>
             </div>
           </div>
@@ -115,20 +122,25 @@ export const Projects = () => {
         <Link href={'http://biosec.com.ng/'} target={'_blank'}>
           <div className="bg-gray-50 max-w-[450px] cursor-pointer rounded-md shadow-md overflow-hidden mb-10 mx-auto space-y-2">
             <Image
-              src={'/img/biosec.png'}
+              src={'/img/biosecsolutions.png'}
               width={450}
               height={300}
               layout={'responsive'}
               className="shadow-md"
+              alt="Biosec Solutions"
             />
 
             <div className="block p-5">
               <h2 className="text-blue-600 text-lg font-semibold mb-3">
                 Biosec Solutions
               </h2>
-              <p className="text-gray-400">
-                Official website of biosec solutions an identity management
-                company based in nigeria.
+              <p className="text-gray-400 font-normal">
+                Biosec Solutions is an identity management consultancy with core
+                biometrics, smart card, token based enterprise architecture
+                strengths with cummulative experience of over 30 years. <br />{' '}
+                <br /> The official website displays information about offered
+                services, clients and affiliates, projects and products of
+                Biosec Solutions.
               </p>
             </div>
           </div>

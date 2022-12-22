877f659187218e9af828181c42018fae328761e5
Favour Akomolafe <akomolafefavour@gmail.comm>
2022-12-22 01:28:49 +0100

    coverted card holder to uppercase

--- components/Form.tsx
+++ components/Form.tsx
@@ -79,7 +79,7 @@ export const Form = () => {
       setYear(year.toString())
     }
 
-    setCardHolder(cardHolder)
+    setCardHolder(cardHolder.toUpperCase)
     setCardNumber(cardNumber.toString())
 
     setCvc(cvc.toString())
